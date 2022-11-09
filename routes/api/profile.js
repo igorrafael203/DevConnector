f32d4467c7eca00b3469053fcd323bef3a7f1746
igorrafael203 <igorrafael.b27@outlook.com>
2022-11-09 12:34:03 -0300

    Delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -211,5 +211,22 @@ router.put('/experience', [auth, [
     }
 })
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id })
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id) // Get remove index
+
+        profile.experience.splice(removeIndex, 1)
+        await profile.save()
+
+        res.json(profile)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
 
 module.exports = router
