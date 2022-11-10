e07dc4b1543c5d4971f3ccef9bbd9490d3379fd4
igorrafael203 <igorrafael.b27@outlook.com>
2022-11-10 16:54:10 -0300

    Add and delete comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,8 +8,8 @@ const Profile = require('../../models/Profile')
 const User = require('../../models/User')
 
 // @route   POST api/posts
-// @desc    TEST route
-// @acces   Public
+// @desc    Create a post
+// @access   Private
 router.post('/', [auth, [
     check('text', 'Text is require')
         .not()
@@ -40,7 +40,7 @@ router.post('/', [auth, [
 
 // @route   GET api/posts
 // @desc    Get all posts
-// @acces   Private
+// @access   Private
 router.get('/', auth, async (req, res) => {
     try {
         const posts = await Post.find().sort({ date: -1 })
@@ -53,7 +53,7 @@ router.get('/', auth, async (req, res) => {
 
 // @route   GET api/posts/:id
 // @desc    Get post by ID
-// @acces   Private
+// @access   Private
 router.get('/:id', auth, async (req, res) => {
     try {
         const post = await Post.findById(req.params.id)
@@ -70,7 +70,7 @@ router.get('/:id', auth, async (req, res) => {
 
 // @route   DELETE api/posts/:id
 // @desc    Delete a post
-// @acces   Private
+// @access   Private
 router.delete('/:id', auth, async (req, res) => {
     try {
         const post = await Post.findById(req.params.id)
@@ -94,7 +94,7 @@ router.delete('/:id', auth, async (req, res) => {
 
 // @route   PUT api/posts/like/:id
 // @desc    Like a post
-// @acces   Private
+// @access   Private
 router.put('/like/:id', auth, async (req, res) => {
     try {
         const post = await Post.findById(req.params.id)
@@ -117,7 +117,7 @@ router.put('/like/:id', auth, async (req, res) => {
 
 // @route   PUT api/posts/unlike/:id
 // @desc    Like a post
-// @acces   Private
+// @access   Private
 router.put('/unlike/:id', auth, async (req, res) => {
     try {
         const post = await Post.findById(req.params.id)
@@ -141,7 +141,70 @@ router.put('/unlike/:id', auth, async (req, res) => {
     }
 })
 
+// @route   POST api/posts
+// @desc    Create a post
+// @access   Private
+router.post('/comment/:id', [auth, [
+    check('text', 'Text is require')
+        .not()
+        .isEmpty()
+    ]
+], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
 
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        const post = await Post.findById(req.params.id)
 
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        }
+
+        post.comments.unshift(newComment)
+
+        await post.save()
+
+        res.json(post.comments)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete comment
+// @access   Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+
+        // Pull out comment
+        const comment = post.comments.find(
+            comment => comment.id === req.params.comment_id
+        )
+
+        // Make sure comment exists
+        if (!comment) return res.status(400).json({ msg: 'Comment does not exist' })
+
+        // Check user
+        if (comment.user.toString() !== req.user.id)  return res.status(401).json({ msg: 'Comment does not exist' })
+
+        // Get remove index
+        const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id)
+
+        post.comments.splice(removeIndex, 1)
+
+        await post.save()
+
+        res.json(post.comments)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+});
 
 module.exports = router
