91d77a5319680ba001c95278d66823a96c3f4b10
igorrafael203 <igorrafael.b27@outlook.com>
2022-11-16 14:54:27 -0300

    Rearranging folder

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>
+        App
+      </h1>
+    </Fragment>
+  )
+}
+
+export default App;
